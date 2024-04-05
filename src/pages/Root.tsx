39a6857388d5b58d8bd57d9f6cec6e115c73b73d
DanielSon93 <ths_eksldpf@naver.com>
2024-04-05 18:45:14 +0900

    [feat] Header 수정, 하단 Nav Bar
    1. Header 수정
      - 600px 아래로 내려갈 경우 햄버거 메뉴 사라지게 지정
      - 가운데 있던 로고 왼쪽으로 정렬
    
    2. 하단 Nav Bar
      - 메뉴 5개 나오게 개발
      - 햄버거 메뉴, 게시판, 홈, 출퇴근, 채팅

--- src/pages/Root.tsx
+++ src/pages/Root.tsx
@@ -1,11 +1,17 @@
+import { useContext } from "react";
 import Header from "../components/Header/Header";
 import { Outlet } from "react-router-dom";
+import { ResponsiveContext } from "../contexts/ResponsiveContext";
+import BottomNav from "../components/Header/BottomNav";
 
 export default function Root() {
+  const isMobile = useContext(ResponsiveContext);
+
   return (
     <>
       <Header />
       <Outlet />
+      {isMobile && <BottomNav />}
     </>
   );
 }
