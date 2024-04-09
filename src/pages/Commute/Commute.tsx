de66dcfa39bb0236222e46f11bb33acc011608c5
DanielSon93 <ths_eksldpf@naver.com>
2024-04-09 18:32:23 +0900

    [feat] Backdrop, Overlay Modal 생성
    1. 모달을 여러곳에서 사용할 것 같아 Common쪽에 Overlay만 넘기면 모달이 적용되도록 구현

--- src/pages/Commute/Commute.tsx
+++ src/pages/Commute/Commute.tsx
@@ -1,16 +1,43 @@
+import { useState } from "react";
 import styled from "styled-components";
+import Modal from "../../common/Modal";
+
+const Layout = styled.div`
+  width: 100%;
+  display: flex;
+  justify-content: flex-end;
+`;
+
+const Test = styled.div`
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  width: 50vw;
+  height: 50vh;
+  border-radius: 1rem;
+  padding: 1rem;
+  background-color: var(--color-white);
+`;
+
+const Button = styled.button`
+  width: 200px;
+  padding: 1rem;
+`;
 
 export default function Commute() {
-  const Layout = styled.div`
-    width: 100%;
-    display: flex;
-    justify-content: flex-end;
-  `;
+  const [isModalOpen, setIsModalOpen] = useState<boolean>(false);
 
   return (
     <Layout>
       <main className="main">
         <h1 className="title">출퇴근</h1>
+        <Button onClick={() => setIsModalOpen((prev) => !prev)}>버튼</Button>
+        <Modal isModalOpen={isModalOpen} handleIsModalOpen={setIsModalOpen}>
+          <Test>
+            <div>테스트입니다</div>
+          </Test>
+        </Modal>
       </main>
     </Layout>
   );
