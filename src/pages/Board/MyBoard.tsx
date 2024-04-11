b93cd7fe5eb3798943a0e6ce7c0365e7ac1de646
DanielSon93 <ths_eksldpf@naver.com>
2024-04-12 05:03:07 +0900

    [feat] 검색 UI 구현
    1. 검색 UI 구현

---
+++ src/pages/Board/MyBoard.tsx
@@ -0,0 +1,19 @@
+import styled from "styled-components";
+import MyPost from "../../components/Board/MyPost/MyPost";
+
+const Layout = styled.div`
+  width: 100%;
+  display: flex;
+  justify-content: flex-end;
+`;
+
+export default function MyBoard() {
+  return (
+    <Layout>
+      <main className="main">
+        <h2 className="title">내 게시글</h2>
+        <MyPost />
+      </main>
+    </Layout>
+  );
+}
